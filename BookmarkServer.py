654208d5a19925b36eb7b477c560bd937a3b263b
RuoChen95 <xrc_1995@163.com>
2019-02-20 11:43:05 +0800

    让服务能够同时处理多件任务

--- BookmarkServer.py
+++ BookmarkServer.py
@@ -46,6 +46,11 @@ import os
 import requests
 from urllib.parse import unquote, parse_qs
 
+import threading
+from socketserver import ThreadingMixIn
+
+class ThreadHTTPServer(ThreadingMixIn, http.server.HTTPServer):
+    "This is an HTTPServer that supports thread-based concurrency."
 
 memory = {}
 
@@ -148,5 +153,5 @@ class Shortener(http.server.BaseHTTPRequestHandler):
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8000))
     server_address = ('', port)
-    httpd = http.server.HTTPServer(server_address, Shortener)
+    httpd = ThreadHTTPServer(server_address, Shortener)
     httpd.serve_forever()
